5806a302ffb78a9ecbef17df721240245d69b35b
iantal <antal.micky@yahoo.com>
2020-12-27 14:32:33 +0200

    Spring Boot init

---
+++ mtrx/src/main/java/io/seclens/mtrx/MtrxApplication.java
@@ -0,0 +1,13 @@
+package io.seclens.mtrx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtrxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtrxApplication.class, args);
+	}
+
+}
